693660d520cbe24fb7ca463902fd33121f63756e
Josh Wells <josh@parall.ax>
2022-08-17 17:35:00 +0100

    Added logic for breaking down and image and passing it to blurhash

--- app/Console/Commands/BlurhashTest.php
+++ app/Console/Commands/BlurhashTest.php
@@ -29,8 +29,37 @@ class BlurhashTest extends Command
     public function handle()
     {
         $blurhash = FFI::load(resource_path('ffi/blurhash/encode.h'));
-        // TODO break target image down so blurhash can handle it
-        // $blurhash->blurHashForPixels();
+
+        $imagePath = resource_path('img/pic1.png');
+        $image = imagecreatefrompng($imagePath);
+        [$imageWidth, $imageHeight] = getimagesize($imagePath);
+
+        $imageData = [];
+        for ($y = 0; $y < $imageHeight; $y++) {
+            for ($x = 0; $x < $imageWidth; $x++) {
+                $colour = imagecolorat($image, $x, $y);
+                ['red' => $red, 'green' => $green, 'blue' => $blue] = imagecolorsforindex($image, $colour);
+                $imageData[] = $red;
+                $imageData[] = $green;
+                $imageData[] = $blue;
+            }
+        }
+
+        $imageDataSize = count($imageData);
+        $cData = FFI::new("uint8_t[{$imageDataSize}]");
+        FFI::memcpy($cData, pack('C*', ...$imageData), $imageDataSize);
+
+        $res = $blurhash->blurHashForPixels(
+            4,
+            3,
+            $imageWidth,
+            $imageHeight,
+            $cData,
+            3 * $imageWidth,
+        );
+
+        $this->info('Blurhash:');
+        $this->line($res);
 
         return 0;
     }
